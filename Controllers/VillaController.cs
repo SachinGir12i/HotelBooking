d5eaca96e7566bb733331fccedfdb891467a01d8
sachin giri <girikomal3327@gmail.com>
2026-01-28 21:24:30 +0545

    fixed villa controller which had error in the previous commit and add error pop-ups

--- Controllers/VillaController.cs
+++ Controllers/VillaController.cs
@@ -36,18 +36,60 @@ namespace HotelBooking.Controllers
             {
                 _db.Villas.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Villa created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
         }
-        public IActionResult Update(int villaId)
+
+        
+        public IActionResult Update(int id)
         {
-            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == id);
             if (obj == null)
             {
                 return RedirectToAction("Error","Home");
             }
             return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            
+            if (ModelState.IsValid && obj.Id>0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Villa updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int id)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "Villa deleted successfully";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Error while deleting villa";
+            return View();
         }
     }
 }
