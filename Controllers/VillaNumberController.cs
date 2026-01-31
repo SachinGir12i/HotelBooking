4894d6101887c1d8fa8f620f1a2ce2ea8ebe20e9
sachin giri <girikomal3327@gmail.com>
2026-01-31 10:32:20 +0545

    added villa number in the navigation with create and index page and its necessary configuration in VillaNUmber controller

---
+++ Controllers/VillaNumberController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HotelBooking.Infrastructure.Data;
+using HotelBooking.Domain.Entities;
+
+
+namespace HotelBooking.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                {
+                    ModelState.AddModelError("name", "The Description cannot be the same as the Name.");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Villa created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        
+        public IActionResult Update(int id)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            
+            if (ModelState.IsValid && obj.Id>0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Villa updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int id)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "Villa deleted successfully";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Error while deleting villa";
+            return View();
+        }
+    }
+}
